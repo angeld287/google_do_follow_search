010ecb6cba4a738b2480655e9e569c4d4740fa20
Angel Angeles <daniel_1234@hotmail.es>
2022-07-17 08:47:36 -0400

    fixing dev integrations

--- server/src/providers/Locals.ts
+++ server/src/providers/Locals.ts
@@ -14,7 +14,7 @@ class Locals {
      */
     public static config(): any {
         const port = process.env.PORT || 3001;
-        const DATABASE_URL = process.env.DATABASE_URL || 'postgres://admin:admin@localhost:5432/db';
+        const DATABASE_URL = process.env.DATABASE_URL || 'postgres://admin:admin@localhost:5430/db';
 
         const appSecret = process.env.APP_SECRET || 'secret_key';
         const apiPrefix = process.env.API_PREFIX || 'api';
