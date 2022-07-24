184c70d307b3a6f3c38d1f993b33b41c1dfa30bf
Angel Angeles <daniel_1234@hotmail.es>
2022-07-24 12:01:50 -0400

    adding condicioned tags to views

--- client/src/components/GoogleResultsList/GoogleResultItem/index.tsx
+++ client/src/components/GoogleResultsList/GoogleResultItem/index.tsx
@@ -3,12 +3,13 @@ import { Avatar, List, Row, Tag } from 'antd';
 import React, { useCallback, useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from '../../../app/hooks';
 import { getPageSourceAsync } from '../../../features/pageSource/asyncThunks';
+import { IPageSourceSlice } from '../../../features/pageSource/IPageSource';
 import { selectPageSource } from '../../../features/pageSource/pageSourceSlice';
 import { IGoogleSearchResult } from '../../../interfaces/models/IGoogleSearchResult';
 import CustomButton from '../../CustomButton';
 
 const GoogleResultsList: React.FC<IGoogleSearchResult> = ({ position, snippet, link, title }) => {
-    const pageSource = useAppSelector(selectPageSource);
+    const pageSource: IPageSourceSlice = useAppSelector(selectPageSource);
     const dispatch = useAppDispatch()
 
     useEffect(() => {
@@ -20,7 +21,11 @@ const GoogleResultsList: React.FC<IGoogleSearchResult> = ({ position, snippet, l
     }, [])
 
     const FollowTag = useCallback(() => (pageSource.status === 'pending') ? <LoadingOutlined /> : (
-        <Tag color="#f50">do follow rel</Tag>
+        (pageSource.status === 'failed' ? <Tag color="#f50">Error</Tag> :
+            (pageSource.results?.hasDoFollow ? <Tag color="#87d068">Has do Follow - Count: {pageSource.results.doFollowCount}</Tag> :
+                (<Tag color="#f50">No Has Do Follow</Tag>)
+            )
+        )
     ), [pageSource.status])
 
     return (
