13c9c0144c527b8a919bdbec4619edc66ad56ed0
Angel Angeles <daniel_1234@hotmail.es>
2022-08-02 10:49:29 -0400

    removing unused middlewares

--- server/src/middlewares/Kernel.ts
+++ server/src/middlewares/Kernel.ts
@@ -8,10 +8,6 @@ import { Application } from 'express';
 
 import CORS from './CORS';
 import Http from './Http';
-import Views from './Views';
-import Statics from './Statics';
-import CsrfToken from './CsrfToken';
-import StatusMonitor from './StatusMonitor';
 
 import Locals from '../providers/Locals';
 
@@ -25,18 +21,6 @@ class Kernel {
 
 		// Mount basic express apis middleware
 		_express = Http.mount(_express);
-
-		// Mount csrf token verification middleware
-		_express = CsrfToken.mount(_express);
-
-		// Mount view engine middleware
-		_express = Views.mount(_express);
-
-		// Mount statics middleware
-		_express = Statics.mount(_express);
-
-		// Mount status monitor middleware
-		_express = StatusMonitor.mount(_express);
 
 		return _express;
 	}
